6d3f83b591d94014754bb9b9b30acc027cac2d72
EGOIST <0x142857@gmail.com>
2021-04-16 00:23:55 +0800

    feat: display version number

--- cmd/root.go
+++ cmd/root.go
@@ -7,16 +7,22 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var rootCmd = &cobra.Command{
-	Use:   "doko",
-	Short: "Docker-based local development tool",
-	Run: func(cmd *cobra.Command, args []string) {
-		// Do Stuff Here
-	},
-}
-
 // Execute runs the CLI
 func Execute() {
+	var displayVersion bool
+
+	rootCmd := &cobra.Command{
+		Use:   "doko",
+		Short: "Docker-based local development tool",
+		Run: func(cmd *cobra.Command, args []string) {
+			if displayVersion {
+				fmt.Printf("0.0.6")
+			}
+		},
+	}
+
+	rootCmd.Flags().BoolVarP(&displayVersion, "version", "v", false, "Displays version number")
+
 	rootCmd.AddCommand(listCmd)
 	rootCmd.AddCommand(enableCmd)
 	rootCmd.AddCommand(disableCmd)
