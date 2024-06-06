b0a4dd2ea1a600a440ff019376297c4c0e53d533
EGOIST <0x142857@gmail.com>
2024-06-06 19:21:13 +0800

    feat: add --pgvector support
    BREAKING CHANGE:
    
    require node 20

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-import fs from "fs"
 import { cac } from "cac"
 import { version } from "../package.json"
 import { DEFAULT_PASSWORD } from "./constants"
@@ -13,48 +12,51 @@ import {
 
 const cli = cac(`doko`)
 
-cli.command("start <service>", "Start a service").action(async (name) => {
-  if (isDockerContainerRunning(`doko_${name}`)) {
-    dockerStop(name)
-  }
+cli
+  .command("start <service>", "Start a service")
+  .option("--pgvector", "Enable pgvector for postgres")
+  .action(async (name, flags) => {
+    if (isDockerContainerRunning(`doko_${name}`)) {
+      dockerStop(name)
+    }
 
-  switch (name) {
-    case "postgres":
-      dockerRun({
-        serviceName: "postgres",
-        image: "postgres:14",
-        port: "5432:5432",
-        env: [`POSTGRES_PASSWORD=${DEFAULT_PASSWORD}`],
-        volumes: ["doko_pg:/var/lib/postgresql/data"],
-      })
-      break
-    case "redis":
-      dockerRun({
-        serviceName: "redis",
-        image: "redis:6",
-        port: "6379:6379",
-        env: [`REDIS_PASSWORD=${DEFAULT_PASSWORD}`],
-        volumes: ["doko_redis:/data"],
-      })
-      break
-    case "mysql":
-      dockerRun({
-        serviceName: "mysql",
-        image: "mysql:8",
-        port: "3306:3306",
-        env: [`MYSQL_ROOT_PASSWORD=${DEFAULT_PASSWORD}`],
-        volumes: ["doko_mysql:/var/lib/mysql"],
-      })
-    case "chrome":
-      dockerRun({
-        serviceName: "chrome",
-        image: "browserless/chrome:latest",
-        port: "4742:3000",
-        env: [`CONNECTION_TIMEOUT=600000`],
-      })
-      break
-  }
-})
+    switch (name) {
+      case "postgres":
+        dockerRun({
+          serviceName: "postgres",
+          image: flags.pgvector ? "pgvector/pgvector:pg16" : "postgres:16",
+          port: "5432:5432",
+          env: [`POSTGRES_PASSWORD=${DEFAULT_PASSWORD}`],
+          volumes: ["doko_pg:/var/lib/postgresql/data"],
+        })
+        break
+      case "redis":
+        dockerRun({
+          serviceName: "redis",
+          image: "redis:6",
+          port: "6379:6379",
+          env: [`REDIS_PASSWORD=${DEFAULT_PASSWORD}`],
+          volumes: ["doko_redis:/data"],
+        })
+        break
+      case "mysql":
+        dockerRun({
+          serviceName: "mysql",
+          image: "mysql:8",
+          port: "3306:3306",
+          env: [`MYSQL_ROOT_PASSWORD=${DEFAULT_PASSWORD}`],
+          volumes: ["doko_mysql:/var/lib/mysql"],
+        })
+      case "chrome":
+        dockerRun({
+          serviceName: "chrome",
+          image: "browserless/chrome:latest",
+          port: "4742:3000",
+          env: [`CONNECTION_TIMEOUT=600000`],
+        })
+        break
+    }
+  })
 
 cli.command("stop <service>", "Stop a service").action((name) => {
   dockerStop(name)
